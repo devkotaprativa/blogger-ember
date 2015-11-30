9aaac525f6a2cbe3197eec9e56cf3b7594b12a18
prativa <devkotaprativa@gmail.com>
2015-11-30 09:33:07 +0545

    the returned value of modelfor posts is kept in array and in template its is rendered by looping. the isEditing property is defined in controller and and two actions edit and save is created to toggle this property. according to this property template either displays the title and body or displays both inside form to edit

--- app/routes/post.js
+++ app/routes/post.js
@@ -5,7 +5,8 @@ export default Ember.Route.extend({
 	model: function(params) {
 		// console.log("hell")
 		 console.log(this.modelFor('posts').findBy('id', params.post_id).title)
-		 
-		return this.modelFor('posts').findBy('id', params.post_id)
+		 //TODO
+		 var post = [this.modelFor('posts').findBy('id', params.post_id)];
+		return post;
 	}
 });
